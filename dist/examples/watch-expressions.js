36128176de765c10261934c73f2d9b7bb34b2737
Trevor D. Miller <trevordmiller@icloud.com>
2014-10-13 13:20:15 -0600

    Adding slide content.

---
+++ dist/examples/watch-expressions.js
@@ -0,0 +1,23 @@
+// Yes, this is global. Deal with it.
+
+'use strict';
+
+var groceryList = [
+	'Bread',
+	'Milk',
+	'Eggs',
+	'Fish',
+	'Cereal'
+];
+
+var watchButtonNode = document.querySelector('.watch-button');
+watchButtonNode.addEventListener('click', function() {
+
+	// Create an empty array
+	var groceryCart = [];
+
+	// Push an item into the array during each loop
+	for(var i = 0; i <= 5; i++) {
+		groceryCart.push(groceryList[i]);
+	}
+});
